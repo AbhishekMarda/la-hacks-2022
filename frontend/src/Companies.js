0ae175c830c51462f06c5d5332bc005cecaa9607
Aryan Patel <aroo2001@gmail.com>
2022-04-17 01:32:43 -0700

    UPdates

--- frontend/src/Companies.js
+++ frontend/src/Companies.js
@@ -1,82 +1,163 @@
-import {Link} from "react-router-dom";
-import {Typography, AppBar, Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container} from "@material-ui/core";
-import './styles/Skills.css';
+import { Link } from "react-router-dom";
+import {
+    Typography,
+    AppBar,
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    CardMedia,
+    CssBaseline,
+    Grid,
+    Toolbar,
+    Container,
+} from "@material-ui/core";
+import "./styles/Skills.css";
 import { StylesProvider } from "@material-ui/core";
-import {useState} from "react";
-import {useNavigate} from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
-const cards = [1,2,3,4,5];
+const cards = [1, 2, 3, 4, 5];
 
 function Companies() {
-
+    const [companies, setCompanies] = useState([]);
     const navigate = useNavigate();
+    const location = useLocation();
+    setup();
 
-    const HandleHome =  (event) => {
-        event.preventDefault();
-        navigate("/");
-    };
+    async function getSkills(uri) {
+        const response = await fetch(uri, {
+            mode: "cors",
+        });
+        const data = await response.json();
+        return data;
+    }
+
+    async function getCompanies(uri, skillsJSON) {
+        const response = await fetch(uri, {
+            method: "POST",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: JSON.stringify(skillsJSON),
+        });
+        const data = response.json();
+        return data;
+    }
+
+    async function setup() {
+        let id = "";
+        if (location.state) id = location.state.id;
+        else id = "sanchit1591";
+        const serverUrl = "http://localhost:105";
+        let requestUri = serverUrl + "/getSkillsFromId?id=" + id;
+        getSkills(requestUri)
+            .then((skillsArr) => {
+                let skillsJSON = {
+                    skills: skillsArr,
+                };
+                return skillsJSON;
+            })
+            .then((skillsJSON) => {
+                console.log(skillsJSON);
+                getCompanies(serverUrl + "/getTopCompanies", skillsJSON).then(
+                    (data) => {
+                        // for (let company in data) {
+                        //     setCompanies((arr) => [...arr, { company }]);
+                        // }
+                        // console.log(companies);
+                    }
+                );
+            });
+    }
 
     const HandleMore = (event) => {
         event.preventDefault();
         navigate("/skills");
-    }
+    };
 
     return (
         <div className="Companies">
             <StylesProvider injectFirst>
                 <CssBaseline />
-                    <AppBar position="relative" className="nav" style={{background: "#4877b6"}}>
-                        <Toolbar>
-                            <img src="favicon.ico" alt="Query Compatible" className="logo" onClick={HandleHome}/>
-                            {/* <Typography variant = "h6">
+                <AppBar
+                    position="relative"
+                    className="nav"
+                    style={{ background: "#4877b6" }}
+                >
+                    <Toolbar>
+                        <img
+                            src="favicon.ico"
+                            alt="Query Compatible"
+                            className="logo"
+                        />
+                        {/* <Typography variant = "h6">
                                 <Link to="/companies" className="link">Companies</Link>
                                 <Link to="/skills" className="link">Skills</Link>
                             </Typography> */}
-                        </Toolbar>
-                    </AppBar>
-                    <main>
-                        <div className="container">
-                            <Container maxWidth="sm">
-                                <Typography variant="h2" align="center" color="textPrimary" gutterBottom className="Header">
-                                    Companies
-                                </Typography>
-                            </Container>
-                        </div>
-                        <Container className="cardGrid">
-                            <Grid container spacing={4}>
-                            {cards.map((card) =>(
-                                <Grid item key={card} sm={4} m={4}>
-                                <Card className="card">
-                                    <CardMedia 
-                                    className="cardMedia"
-                                    image="https://source.unsplash.com/random"
-                                    title="Company Name"
-                                    />
-                                    <CardContent className="classes.cardContent">
-                                        <Typography variant="h5" gutterBottom>
-                                            Company description
-                                        </Typography>
-                                        <Typography variant="h5" gutterBottom>
-                                            Job
-                                        </Typography>
-                                    </CardContent>
-                                    <CardActions>
-                                        <Button color="primary" onClick={HandleMore}>
-                                            Learn more about this company!
-                                        </Button> 
-                                    </CardActions>
-                                </Card>
-                            </Grid>
-                            ))}
-                            </Grid>
+                    </Toolbar>
+                </AppBar>
+                <main>
+                    <div className="container">
+                        <Container maxWidth="sm">
+                            <Typography
+                                variant="h2"
+                                align="center"
+                                color="textPrimary"
+                                gutterBottom
+                                className="Header"
+                            >
+                                Companies
+                            </Typography>
                         </Container>
-                    </main>
-                    <footer className="footer">
-                        <Typography>Developed by blah blah blah</Typography>
-                    </footer>
-                </StylesProvider>
+                    </div>
+                    <Container className="cardGrid">
+                        <Grid container spacing={4}>
+                            {cards.map((card) => (
+                                <Grid item key={card} sm={4} m={4}>
+                                    <Card className="card">
+                                        <CardMedia
+                                            className="cardMedia"
+                                            image="https://source.unsplash.com/random"
+                                            title="Company Name"
+                                        />
+                                        <CardContent className="classes.cardContent">
+                                            <Typography
+                                                variant="h5"
+                                                gutterBottom
+                                            >
+                                                Company description
+                                            </Typography>
+                                            <Typography
+                                                variant="h5"
+                                                gutterBottom
+                                            >
+                                                Job
+                                            </Typography>
+                                        </CardContent>
+                                        <CardActions>
+                                            <Button
+                                                color="primary"
+                                                onClick={HandleMore}
+                                            >
+                                                Learn more about this company!
+                                            </Button>
+                                        </CardActions>
+                                    </Card>
+                                </Grid>
+                            ))}
+                        </Grid>
+                    </Container>
+                </main>
+                <footer className="footer">
+                    <Typography>Developed by blah blah blah</Typography>
+                </footer>
+            </StylesProvider>
         </div>
-    )
+    );
 }
 
 export default Companies;
--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,79 +1,105 @@
 import React from "react";
-import {useState} from "react";
-import {useNavigate} from "react-router-dom";
-import { Input, Typography, Button, CssBaseline, AppBar, Toolbar} from "@material-ui/core";
-import {Send} from '@material-ui/icons';
-import './styles/Home.css';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+    Input,
+    Typography,
+    Button,
+    CssBaseline,
+    AppBar,
+    Toolbar,
+} from "@material-ui/core";
+import { Send } from "@material-ui/icons";
+import "./styles/Home.css";
 
 function Home() {
-    
     const navigate = useNavigate();
 
-    const HandleHome =  (event) => {
-      navigate("/");
-  };
-    
+    const HandleHome = (event) => {
+        navigate("/");
+    };
 
-    const [inputs, setInputs] = useState( {
+    const [inputs, setInputs] = useState({
         link: "",
         job: "",
     });
     const [submit, setSubmit] = useState("false");
-  
-    const HandleLinkInputChange = (event) => {
-        setInputs({...inputs, link: event.target.value});
-    }
 
+    const HandleLinkInputChange = (event) => {
+        setInputs({ ...inputs, link: event.target.value });
+    };
 
     const HandleJobInputChange = (event) => {
-        setInputs({...inputs, job: event.target.value});
-    }
-  
+        setInputs({ ...inputs, job: event.target.value });
+    };
+
     const HandleSubmit = (event) => {
-      event.preventDefault();
-      setSubmit(true);
-      navigate("/companies");
-    }
-  
-  
+        event.preventDefault();
+        setSubmit(true);
+        let id = event.target.linkedinId.value;
+        
+        navigate("/companies", { state: { id: id} });
+    };
+
     return (
-      <div className="Home">
-        <CssBaseline/>
-        <div className="imageContainer">
-          <img src="logo_blue_crop.png" alt="Query Compatible" className="image" onClick={HandleHome} style={{}}/>
-        </div>
-        <main>
-          <div className="container">
-            <Typography variant ="h1" className="Header">Find Jobs with Query Compatible</Typography>
-            <Typography variant = "h4" className="subHeader">Make sure that your Linkedin is public!</Typography>
-            <div className= "form-container" >
-              <form onSubmit={HandleSubmit}>
-                <Input className="input"
-                  onChange = {HandleLinkInputChange}
-                  value = {inputs.link} 
-                  fullWidth
-                  type="text" 
-                  placeholder="Drop your Linkedin url here">
-                </Input> 
-                <Input className="input"
-                  onChange = {HandleJobInputChange}
-                  value = {inputs.job} 
-                  fullWidth
-                  variant="outlined"
-                  type="text" 
-                  placeholder="What role are you applying for?">
-                </Input>
-                <Button variant="contained" type="submit"endIcon={<Send/>} style={{width:"360px", marginTop: "50px"}}className="button">
-                Send
-                </Button>
-              </form>
+        <div className="Home">
+            <CssBaseline />
+            <div className="imageContainer">
+                <img
+                    src="logo_blue_crop.png"
+                    alt="Query Compatible"
+                    className="image"
+                    onClick={HandleHome}
+                    style={{}}
+                />
             </div>
-          </div>
-        </main>
-        <footer className="footer">
-          <Typography style={{align:"center"}}>Developed by blah blah blah</Typography>
-        </footer>
-      </div>
+            <main>
+                <div className="container">
+                    <Typography variant="h1" className="Header">
+                        Find Jobs with Query Compatible
+                    </Typography>
+                    <Typography variant="h4" className="subHeader">
+                        Make sure that your Linkedin is public!
+                    </Typography>
+                    <div className="form-container">
+                        <form onSubmit={HandleSubmit}>
+                            <Input
+                                name="linkedinId"
+                                className="input"
+                                onChange={HandleLinkInputChange}
+                                value={inputs.link}
+                                fullWidth
+                                type="text"
+                                placeholder="Drop your Linkedin url here"
+                            ></Input>
+                            <Input
+                                className="input"
+                                onChange={HandleJobInputChange}
+                                value={inputs.job}
+                                fullWidth
+                                variant="outlined"
+                                type="text"
+                                placeholder="What role are you applying for?"
+                            ></Input>
+                            <Button
+                                variant="contained"
+                                type="submit"
+                                endIcon={<Send />}
+                                style={{ width: "360px", marginTop: "50px" }}
+                                className="button"
+                            >
+                                Send
+                            </Button>
+                        </form>
+                    </div>
+                </div>
+            </main>
+            <footer className="footer">
+                <Typography style={{ align: "center" }}>
+                    Developed by blah blah blah
+                </Typography>
+            </footer>
+        </div>
     );
 }
 
